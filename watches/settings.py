94af407faa185c4427c91eb3878fee35a3948425
ismoil1jr <isroile70@gmail.com>
2026-02-03 19:57:39 +0500

    first commit

--- watches/settings.py
+++ watches/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-uh-*=yb76!zz2jgk7w1(r)wlnp@ak^g&bwhtoj^jfx=$oyzi93
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
